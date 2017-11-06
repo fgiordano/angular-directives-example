6a46b525f4447a726bc1080efe058f563efc37ce
Francesca Giordano <francesca020406@hotmail.com>
2017-11-06 15:52:33 -0500

    learning directves

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgForExampleComponent
   ],
   imports: [
     BrowserModule
